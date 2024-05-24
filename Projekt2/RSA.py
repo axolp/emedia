71f53bd862e685726cce59c0b6aea698570cb734
axolp <j.w.ploska@gmail.com>
2024-05-24 12:15:08 +0200

    feat: podzial na bloki, szyfrowanie, padding, deszyfrowanie

--- Projekt2/RSA.py
+++ Projekt2/RSA.py
@@ -1,38 +1,50 @@
 class RSA:
     
     def mod_inverse(self, e, phi):
-        for d in range(3, phi):
-            if (d*e)%phi == 1:
-                return d
-            
+        # Rozszerzony Algorytm Euklidesa
+        x0, x1, y0, y1 = 0, 1, 1, 0
+        phi0 = phi
+        while e != 0:
+            q = phi // e
+            phi, e = e, phi % e
+            x0, x1 = x1, x0 - q * x1
+            y0, y1 = y1, y0 - q * y1
+        if phi != 1:
+            return None  
+        else:
+            return x0 % phi0  
+
     def __init__(self, p, q):
         self.p = p
         self.q = q
-        self.n = p * q # moze tylko prawidlwoo kodawc liczby mniejsze od n
+        self.n = p * q
         self.phi = (p - 1) * (q - 1)
-        self.e = 7 
-        self.d = self.mod_inverse(self.e, self.phi)  # Adjusted private key calculation
+        self.e = 65537  # typowa
+        self.d = self.mod_inverse(self.e, self.phi)
+        
+        if self.d is None:
+            raise ValueError("Nie można znaleźć odwrotności modularnej. Sprawdź wartości p, q, e.")
         
         self.public_key = [self.e, self.n]
-        self.private_key= [self.d, self.n]
+        self.private_key = [self.d, self.n]
 
     def encrypt(self, m, public_key):
-        e= public_key[0]
-        n= public_key[1]
-        return pow(m, e)%n
+        e, n = public_key
+        return pow(m, e, n)
     
     def decrypt(self, encrypted_message):
         return pow(encrypted_message, self.d, self.n)
-    
 
-    
-Alice= RSA(5,11)    
-Bob = RSA(17, 11)
 
-#Alice wysyla do Boba
-cipher_text= Alice.encrypt(10, Bob.public_key)
-print(Bob.decrypt(cipher_text))
+Alice = RSA(1299821, 1299827)  
+Bob = RSA(1299811, 1299817) # n to 1 689 516 434 587 pozwala na blok o dlugosci 6
 
-#Bob wysyla do Alice
-cipher_text= Bob.encrypt(25, Alice.public_key)
-print(Alice.decrypt(cipher_text))
+# Alice wysyła do Boba
+cipher_text = Alice.encrypt(70, Bob.public_key)
+#print("zaszyfrowane: ", cipher_text)
+#print("odszyfrowane: ", Bob.decrypt(cipher_text))
+
+# Bob wysyła do Alice
+cipher_text2 = Bob.encrypt(707070707070, Alice.public_key)
+#print("zaszyfrowane: ", cipher_text2)
+#print("odszyfrowane: ", Alice.decrypt(cipher_text2))
---
+++ Projekt2/png_proj2.py
@@ -0,0 +1,167 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import RSA
+
+
+def readMetaData(file_name):
+    with open(file_name, 'rb') as file:
+        return file.read()
+
+def writeMetaData(file_name, data):
+    with open(file_name, 'wb') as file:
+        file.write(data)  
+
+def find_all(haystack, needle):
+    start = 0
+    while True:
+        start = haystack.find(needle, start)
+        if start == -1:  
+            break
+        yield start
+        start += len(needle) 
+
+#def into_block_into_ascii(n, data):
+
+
+
+def change_length_insert_ciphered_data(ciphered_data):
+    n= len(ciphered_data) #//2
+    idat= ""
+    if n!= 0:
+        data_len= str(hex(n//2)[2:]).zfill(8)#dziele przez dwa bo ma byc liczba 
+        print("data len in hex: ",data_len)
+        idat= data_len + "49444154" + ciphered_data + "00000000"   #na koncu crc
+       
+    return idat
+def from_ascii_to_hex(data):
+    hex_codes= {
+    '65': 'A',
+    '66': 'B',
+    '67': 'C',
+    '68': 'D',
+    '69': 'E',
+    '70': 'F',
+    '48': '0',
+    '49': '1',
+    '50': '2',
+    '51': '3',
+    '52': '4',
+    '53': '5',
+    '54': '6',
+    '55': '7',
+    '56': '8',
+    '57': '9',
+    '99': 'break'
+    }
+    data= str(data)
+    #print(data)
+    decyphered_data= ""
+    for i in range(0, len(data)-1, 2):
+        pair= data[i]+data[i+1]
+        #print(pair)
+        hex= hex_codes[pair]
+        if hex == "break":
+            break
+        decyphered_data+=hex
+
+    return decyphered_data
+
+def turn_to_ASCII(data):
+    
+    ascii_data= ""
+    ascii_codes = {
+    'A': 65,
+    'B': 66,
+    'C': 67,
+    'D': 68,
+    'E': 69,
+    'F': 70,
+    '0': 48,
+    '1': 49,
+    '2': 50,
+    '3': 51,
+    '4': 52,
+    '5': 53,
+    '6': 54,
+    '7': 55,
+    '8': 56,
+    '9': 57
+    #dodaje dodatkowy znak 99 dla paddingu
+    }
+    for b in data:
+        #print(b)
+        ascii_data+= str(ascii_codes[b.upper()])
+    #print("ascii data: ", ascii_data)
+    
+    return int(ascii_data)
+    
+
+def encrypt_png_data(data, block_n):
+    global encoder, decoder
+    encrypted_data= ""
+    padding_idx= len(data) - (len(data)%block_n) 
+    leftovers= len(data)%block_n
+    for i in range(1, len(data)):
+        if i == padding_idx:
+            #zastosuj padiing
+            break
+
+        if i % block_n == 0:
+            block_data= data[i-block_n:i]
+            #print("block data", block_data)
+            ascii_data= turn_to_ASCII(block_data)
+            encrypted_block= encoder.encrypt(ascii_data, decoder.public_key)
+            decrypted_block= decoder.decrypt(int(encrypted_block))
+            print("ascii data:", ascii_data, "encrypted: ", encrypted_block, "decrypted_ascii: ", decrypted_block, "decrypoted_hex: ", from_ascii_to_hex(decrypted_block))
+            encrypted_data+= str(encrypted_block)
+
+    print("padding idx:", padding_idx,"leftovers: ", leftovers)
+    if leftovers != 0:
+        padding_data= data[padding_idx:padding_idx+leftovers]
+        print("padding data:", padding_data)
+        ascii_padding= str(turn_to_ASCII(padding_data))
+        for j in range(block_n - leftovers):
+            ascii_padding+="99"
+        
+        encrypted_padding= encoder.encrypt(int(ascii_padding), decoder.public_key)
+        encrypted_data+= str(encrypted_padding)
+
+    return encrypted_data
+
+def rsa_image():
+  
+    binary_file = readMetaData("image.png")
+    hex_data = binary_file.hex()
+
+    indexes = list(find_all(hex_data, '49444154'))
+    middle= ""
+
+    left_idx= indexes[0]
+    left= hex_data[:left_idx-8]
+
+    right_idx= hex_data.find('49454e44')
+    right= hex_data[right_idx-8:]
+
+    
+
+    for i, idx in enumerate(indexes):
+        #zamien dane z idat na zaszyfrowane dane
+        data_len= int(hex_data[idx-8:idx], 16)*2 #ilosc znakow 16 do oczytania 
+        #print("next idat:", hex_data[idx+8+data_len+8:idx+8+data_len+8+8+8])
+        idat_data= hex_data[idx+8:idx+8+data_len]
+        encrypted_data= encrypt_png_data(idat_data, 6)
+        middle+= change_length_insert_ciphered_data(encrypted_data)
+    new_hex_data= left + middle + right
+   
+          
+    #print("position 8032007", new_hex_data[8032007])
+    new_binary_data = bytes.fromhex(new_hex_data)
+    writeMetaData("ciphered_img.png", new_binary_data)
+
+
+
+
+encoder = RSA.RSA(1299821, 1299827)  
+decoder = RSA.RSA(1299811, 1299817) # n to 1 689 516 434 587 pozwala na blok o dlugosci 6
+rsa_image()
